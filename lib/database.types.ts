585a0c3dcf5731f5da46b050c036c1a20c52da12
spangbaryn <espangenberg@gmail.com>
2024-11-25 13:20:36 -0500

    eslinting

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -6,19 +6,44 @@ export type Json =
   | { [key: string]: Json | undefined }
   | Json[]
 
+interface TableRow {
+  created_at?: string
+  id?: string
+  [key: string]: Json | undefined
+}
+
+interface ViewRow {
+  [key: string]: Json | undefined
+}
+
+interface FunctionResult {
+  [key: string]: Json | undefined
+}
+
+type EnumValue = string
+
 export interface Database {
   public: {
     Tables: {
-      [key: string]: any
+      [key: string]: {
+        Row: TableRow
+        Insert: TableRow
+        Update: Partial<TableRow>
+      }
     }
     Views: {
-      [key: string]: any
+      [key: string]: {
+        Row: ViewRow
+      }
     }
     Functions: {
-      [key: string]: any
+      [key: string]: {
+        Args: Record<string, unknown>
+        Returns: FunctionResult
+      }
     }
     Enums: {
-      [key: string]: any
+      [key: string]: EnumValue[]
     }
   }
 }
