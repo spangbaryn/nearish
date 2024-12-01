477ffca3af4b0a2119ec173a1d35e4f01fde9d11
spangbaryn <espangenberg@gmail.com>
2024-12-01 12:20:13 -0500

    day picker fix

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DropdownProps } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -54,7 +54,7 @@ function Calendar({
       components={{
         IconLeft: () => <ChevronLeft className="h-4 w-4" />,
         IconRight: () => <ChevronRight className="h-4 w-4" />,
-      } as DropdownProps["components"]}
+      } as unknown as any}
       {...props}
     />
   )
