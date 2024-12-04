c1986434cfadbec34d85a908b717a9f7cfdb9672
spangbaryn <espangenberg@gmail.com>
2024-12-03 22:45:49 -0500

    sign up type safety

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -110,7 +110,7 @@ export default function SignUpPage() {
                 type="email"
                 placeholder="name@example.com"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
                 required
               />
             </div>
@@ -120,7 +120,7 @@ export default function SignUpPage() {
                 id="password"
                 type="password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
                 required
               />
             </div>
