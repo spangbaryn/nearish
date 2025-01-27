2f7bac3e3dc7b4d845e049b3f97ba50d2f2f06c7
spangbaryn <espangenberg@gmail.com>
2025-01-27 16:16:09 -0500

    sail

--- app/api/businesses/[id]/invites/route.ts
+++ app/api/businesses/[id]/invites/route.ts
@@ -2,13 +2,9 @@ import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-type Params = {
-  id: string;
-};
-
 export async function GET(
   request: Request,
-  { params }: { params: Params }
+  { params }: { params: { [key: string]: string } }
 ) {
   try {
     const supabase = createRouteHandlerClient({ cookies })
